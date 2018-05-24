1a86e5a2d1739db327c87f2c0ad82a197fb28e7f
hondo-s <hondo-s@klab.com>
2018-05-24 23:25:21 +0900

    footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import ChatInput from './ChatInput'
 
-import './App.styl';
+import './App.styl'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to dApps</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChatInput />
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/ChatInput.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default class ChatInput extends React.Component {
+  render() {
+      return (
+        <footer className="teal">
+          <form className="container">
+            <div className="row">
+              <div className="input-field col s10">
+                <i className="prefix material-icons">chat</i>
+                <input type="text" placeholder="Type your message" />
+                <span className="chip left">
+                  <img src="//robohash.org/503483?set=set2&bgset=bg2&size=70x70" />
+                  <span>Anonymous robot #503483</span>
+                </span>
+              </div>
+              <div className="input-field col s2">
+                <button type="submit" className="waves-effect waves-light btn-floating btn-large">
+                  <i className="material-icons">send</i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </footer>
+      )
+  }
+}
